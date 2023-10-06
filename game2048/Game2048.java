36a5500e50796c985cfa0e605c8d1b2e300879a5
Evg_laptop <sirius-tyrin@yandex.ru>
2023-10-06 16:45:52 -0400

    GAME: 2048 New Method : createGame()

--- game2048/Game2048.java
+++ game2048/Game2048.java
@@ -4,8 +4,13 @@ import com.javarush.engine.cell.*;
 
 public class Game2048 extends Game {
     private static final int SIDE = 4;
+    private int[][]gameField = new int[SIDE][SIDE];
 @Override
     public void initialize() {
         setScreenSize(4, 4);
+        createGame();
+    }
+    private void createGame(){
+
     }
 }
