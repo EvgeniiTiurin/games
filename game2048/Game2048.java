7d8ea55d3f3eda962e255f120293a92e57dcc1e9
Evg_laptop <sirius-tyrin@yandex.ru>
2023-10-19 11:58:34 -0400

    GAME: 2048 New Method : createNewNumber()

--- game2048/Game2048.java
+++ game2048/Game2048.java
@@ -9,8 +9,44 @@ public class Game2048 extends Game {
     public void initialize() {
         setScreenSize(4, 4);
         createGame();
+        drawScene();
     }
     private void createGame(){
+        createNewNumber();
+        createNewNumber();
+        showMatrix();
+    }
 
+    private void drawScene() {
+        for (int x = 0; x < SIDE; x++){
+            for (int y = 0; y < SIDE; y++){
+            //setCellColor(x, y, Color.RED);
+            }
+        }
+    }
+
+    private void createNewNumber() {
+        boolean notNull = true;
+        while(notNull) {
+            int x = getRandomNumber(SIDE);
+            int y = getRandomNumber(SIDE);
+            if(gameField[y][x] == 0) {
+                if (getRandomNumber(10) == 9) {
+                    gameField[y][x] = 4;
+                }
+                else {
+                    gameField[y][x] = 2;
+                }
+                notNull = false;
+            }
+        }
+    }
+
+    private void showMatrix() {
+        for (int x = 0; x < SIDE; x++){
+            for (int y = 0; y < SIDE; y++){
+                System.out.print(gameField[y][x] + " ");
+            }
+        }
     }
 }
